c1f89ccf0716f9b032dedfe1edbc64f719c50894
Myounghoon Lee <lmhchris47@snu.ac.kr>
2022-07-08 09:45:52 +0900

    url 수정

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/movie/:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/react-movie-app/movie/:id" element={<Detail />} />
+        <Route path="/react-movie-app" element={<Home />} />
       </Routes>
     </Router>
   );
